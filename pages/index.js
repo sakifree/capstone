845967bc89559709e7b04accad39cd1801c9c4ea
Sakinah Freeman <freemansakinah@gmail.com>
2023-03-22 21:07:32 -0400

    checks api routes with Postman

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import { getPosts } from "@/utils/actions"
+
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
     </>
   )
+}
+
+export async function getServerSideProps(context){
+  const posts = JSON.parse(JSON.stringify(await getPosts()))
+  //console.log(posts)
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
