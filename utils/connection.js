9dfaec1a2cf475edef0800ea3196c4f5e4505d01
Sakinah Freeman <freemansakinah@gmail.com>
2023-03-23 23:41:26 -0400

    fixing deployment issues

--- utils/connection.js
+++ utils/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 import mongoose from "mongoose";
 
 console.log(typeof process.env.MONGO_URI)
+console.log(process.env.MONGO_URI)
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
