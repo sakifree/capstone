976d2e0d7921570a6ee7a33a1e09e7dcf0d4cfef
Sakinah Freeman <freemansakinah@gmail.com>
2023-03-23 23:32:22 -0400

    fixes some things

--- utils/connection.js
+++ utils/connection.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
 require('dotenv').config()
+import mongoose from "mongoose";
 
-console.log(process.env.MONGO_URI)
+console.log(typeof process.env.MONGO_URI)
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
