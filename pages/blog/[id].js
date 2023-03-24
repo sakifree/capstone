6704aa9ed2ae54f9f6ce2c2c3c1b1d5e29af0f1f
Sakinah Freeman <freemansakinah@gmail.com>
2023-03-23 22:56:31 -0400

    individual posts can be shown, updated, and deleted

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -5,8 +5,55 @@ import { useState } from "react"
 
 const Show = ({post}) => {
 
+    const router = useRouter()
+
+    const [form, setForm] = useState({
+        "title": `${post.title}`,
+        "text": `${post.text}`,
+        "img": `${post.img}`
+    })
+
+    const handleChange = (event) => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const handleUpdate = async (event) => {
+        event.preventDefault()
+        await fetch(`/api/post/${post._id}`, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        })
+
+        router.reload(window.location.pathname)
+    }
+
+    const handleDelete = async (event) => {
+        event.preventDefault()
+        await fetch(`/api/post/${post._id}`, {
+            method: "delete"
+        })
+        
+        router.push("/")
+    }
+
     return (
-        <h1>Show Page</h1>
+        <div>
+            <Link href="/"><button>Home</button></Link>
+            <h1>{post.title} Page</h1>
+            <p>{post.text}</p>
+            <form onSubmit={handleUpdate}>
+                <input defaultValue={post.title} type="text" name="title" onChange={handleChange} />
+                <input defaultValue={post.text} type="text" name="text" onChange={handleChange} />
+                <input type="submit" value={`Update ${post.title}`} />
+            </form>
+
+            <form onSubmit={handleDelete}>
+                <input type="submit" value={`Delete ${post.title}`} /> 
+            </form>
+        </div>
     )
 
 } 
