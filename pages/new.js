cbeb95e5107249dbd9fca6d7b4064a0e3c8f147c
Sakinah Freeman <freemansakinah@gmail.com>
2023-03-29 23:56:09 -0400

    more styling

--- pages/new.js
+++ pages/new.js
@@ -39,7 +39,7 @@ const New = (props) => {
         //         <input type="submit" value="Create Post" />
         // </form>
 
-        <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+        <form className="w-full max-w-6xl mx-4 my-4" onSubmit={handleSubmit}>
             <div className="md:flex md:items-center mb-6">
                 <div className="md:w-1/3">
                     <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
@@ -67,7 +67,7 @@ const New = (props) => {
                     </label>
                 </div>
                 <div className="md:w-2/3">
-                    <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" name="text" value={form.text} onChange={handleChange} />
+                    <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" rows="10" cols="20" type="text" name="text" value={form.text} onChange={handleChange} placeholder="What's up.. " />
                 </div>
             </div>
             <div className="md:flex md:items-center mb-6">
