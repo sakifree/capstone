8e24919c4fd8b9499a2c6c24030e8feb32f1e936
Sakinah Freeman <freemansakinah@gmail.com>
2023-03-30 22:20:11 -0400

    adds new font

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import 'tailwindcss/tailwind.css'
 import { SessionProvider } from "next-auth/react"
 import Nav from '@/components/Nav'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps: {session, ...pageProps} }) {
   
   return (
     <SessionProvider session={session}>
+      <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
+          <title>LockdIn</title>
+          <meta name="description" contents="This is a blog social media site." />
+      </Head>
       <Nav/>
       <Component {...pageProps} />
       </SessionProvider>
